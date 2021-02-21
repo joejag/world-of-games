75359c931f69c4bbce9dc8bf0b35ac23ab30cb29
joe.wright <joe@joejag.com>
2021-02-21 03:57:31 +0000

    add joint padding and margin classes

--- client/src/Start.tsx
+++ client/src/Start.tsx
@@ -8,54 +8,46 @@ function Start() {
 
       <div className="flex-row flex-justify mb5">
         <img
-          className="border-tl pt3 pr0 pb3 pl3 bg-white"
+          className="rounded-tl bg-white py3 pr0 pl3"
           src="/images/contestant.png"
           alt=""
           height="300px"
           width="290px"
         />
-        <div className="border-rb bg-white pt5 pb5 pr3 pl3">
+        <div className="rounded-rb bg-white py5 px3">
           <h2 className="m0">Join a quiz</h2>
-          <div className="flex-col mb1 ml5 mr5">
+          <div className="flex-col mb1 mx5">
             <label htmlFor="contestant-name" className="mt3 mb2">
               Your Name
             </label>
-            <input id="contestant-name" className="pr3 pl3 br1 lh5 min-w11 bg-gray-90" />
+            <input id="contestant-name" className="px3 rounded1 lh5 min-w11 bg-gray-90" />
           </div>
-          <div className="flex-col mb1 ml5 mr5">
+          <div className="flex-col mb1 mx5">
             <label htmlFor="code" className="mt3 mb2">
               Quiz code
             </label>
-            <input id="code" className="pr3 pl3 br1 lh5 min-w11 bg-gray-90" />
+            <input id="code" className="px3 rounded1 lh5 min-w11 bg-gray-90" />
           </div>
 
-          <div className="flex-col ml5 mr5">
-            <button className="mt4 pt3 pb3 pr4 pl4 br1 text-upper text-white bg-pink b-pink-90">Join the quiz</button>
+          <div className="flex-col mx5">
+            <button className="mt4 py3 rounded1 text-upper text-white bg-pink b-pink-90">Join the quiz</button>
           </div>
         </div>
       </div>
 
       <div className="flex-row flex-justify">
-        <img
-          className="border-tl pt3 pr0 pb3 pl3 bg-white"
-          src="/images/hosts.jpeg"
-          alt=""
-          height="300px"
-          width="290px"
-        />
-        <div className="border-rb bg-white pt5 pb5 pr3 pl3">
+        <img className="rounded-tl bg-white py3 pr0 pl3" src="/images/hosts.jpeg" alt="" height="300px" width="290px" />
+        <div className="rounded-rb bg-white py5 px3">
           <h2 className="m0">Host a quiz</h2>
-          <div className="flex-col mb1 ml5 mr5">
+          <div className="flex-col mb1 mx5">
             <label htmlFor="name" className="mt3 mb2">
               Your Name
             </label>
-            <input id="name" className="pr3 pl3 br1 lh5 min-w11 bg-gray-90" />
+            <input id="name" className="px3 rounded1 lh5 min-w11 bg-gray-90" />
           </div>
 
-          <div className="flex-col ml5 mr5">
-            <button className="mt4 pt3 pb3 pr4 pl4 br1 text-upper text-white bg-pink b-pink-90">
-              Host your own quiz
-            </button>
+          <div className="flex-col mx5">
+            <button className="mt4 py3 rounded1 text-upper text-white bg-pink b-pink-90">Host your own quiz</button>
           </div>
         </div>
       </div>
